e57d2025786ea6205554b04e66788a6d8401ef52
tfong <phongdicho83@gmail.com>
2025-09-29 18:16:34 +0700

    [feature] render popular movies on first home load

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import { img } from "../utils/tmdb";
+import "./MovieCard.css";
+
+export default function MovieCard({ movie }) {
+  return (
+    <Link to={`/movie/${movie.id}`} className="card">
+      {movie.poster_path ? ( // If poster_path is available, show it, else show placeholder
+        <img
+          src={img(movie.poster_path, "w342")}
+          alt={movie.title}
+          loading="lazy"
+        />
+      ) : (
+        <div className="placeholder" />
+      )}
+      <div className="card-body">
+        <h3>{movie.title}</h3>
+        {movie.vote_average > 0 && (
+          <p className="muted">⭐ {movie.vote_average.toFixed(1)}</p>
+        )}
+      </div>
+    </Link>
+  );
+}
